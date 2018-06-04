66cc9d93896e6afbbf5fc07e99e19365fdbc5e72
jagadeesh <jagadeeshwarcs@gmail.com>
2018-06-04 17:52:31 +0530

    Changed the HTML for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%gy1k!lsxihjqfm(h^4m)--x3s(li30liw-2@z90y*+0^0#r)9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
